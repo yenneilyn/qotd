23c18f1d432b0d8139300e4736f4248600889844
yenneilyn <yenneilyn@hotmail.com>
2014-12-07 22:16:48 -0500

    created quoteService

--- grails-app/controllers/qotd/QuoteController.groovy
+++ grails-app/controllers/qotd/QuoteController.groovy
@@ -3,20 +3,11 @@ package qotd
 class QuoteController {
     static scaffold = true
 
-    def index() {
-        redirect(action: random)
-    }
+    def quoteService
 
-    def random = {
-       def allQuotes = Quote.list()
-        def randomQuote
-        if (allQuotes.size() > 0) {
-            def randomIdx = new Random().nextInt(allQuotes.size())
-            randomQuote = allQuotes[randomIdx]
-        }
-        else{
-            randomQuote = new qotd.Quote(author: "Anonymous", content: "Real Programmers Don't eat Quiche")
-        }
+    def random() {
+       def randomQuote = quoteService.getRandomQuote()
         [quote : randomQuote]
     }
+
 }
---
+++ grails-app/services/qotd/QuoteService.groovy
@@ -0,0 +1,26 @@
+package qotd
+
+import grails.transaction.Transactional
+
+@Transactional
+class QuoteService {
+
+    boolean transactional = false
+
+    def getStaticQuote() {
+        return new Quote(author: "Anonymous", content: "Real Programmers Don't eat quiche")
+    }
+
+    def getRandomQuote() {
+        def allQuotes = Quote.list()
+        def randomQuote
+        if (allQuotes.size() > 0) {
+            def randomIdx = new Random().nextInt(allQuotes.size())
+            randomQuote = allQuotes[randomIdx]
+        }
+        else{
+            randomQuote = getStaticQuote()
+        }
+        return randomQuote
+    }
+}
